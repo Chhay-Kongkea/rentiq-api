12b90d4d30acce1ce78a0d16cd81777d64f6e0ee
Kongkea9 <chhaykongkea9@gmail.com>
2026-09-01 08:26:53 +0700

    config item

--- src/main/java/co/istad/rentiq_api/features/item/dto/request/AdminItemFilter.java
+++ src/main/java/co/istad/rentiq_api/features/item/dto/request/AdminItemFilter.java
@@ -4,6 +4,7 @@ import co.istad.rentiq_api.features.item.enums.ItemApprovalStatus;
 import jakarta.validation.constraints.Size;
 
 public record AdminItemFilter(
+
         @Size(max = 200, message = "Keyword cannot exceed 200 characters")
         String keyword,
         ItemApprovalStatus approvalStatus
